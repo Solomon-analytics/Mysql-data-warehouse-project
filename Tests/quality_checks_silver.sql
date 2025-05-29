542af4d4e0761b0bd318989267e88a7ac0b4ffd4
Solomon Yakubu <129880172+Solomon-analytics@users.noreply.github.com>
2025-05-29 13:42:03 +0100

    Update quality_checks_silver.sql

--- Tests/quality_checks_silver.sql
+++ Tests/quality_checks_silver.sql
@@ -16,49 +16,111 @@ Usage Notes:
 ======================================================================================================
 */
 
--- Exploring: data quality issues
--- Check for Nulls or Duplicates in Primary Key
--- Expectation: No Result
+-- Data Trasnaformation & Data Cleansing
 
-Select *
+
+SELECT *
+FROM bronze.crm_cust_info
+WHERE cst_id = 29466;
+
+-- ranking by cst_create_date
+
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info
+WHERE cst_id = 29466;
+
+-- using the same logic to explore data
+
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 FROM bronze.crm_cust_info;
 
--- Investigating the cst_id
+-- outcome shows there are cst_id which are duplicates because it returns (1,2,3) for flag_last
 
-SELECT Cst_id,
-COUNT(*)
+SELECT *
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info) t
+WHERE flag_last != 1;
+-- outcome reveals dataset contails 8 rows of duplicates relating to 6 distinct cst_id
+
+-- Transforming: duplicate issues
+SELECT *
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 FROM bronze.crm_cust_info
-GROUP BY cst_id
-HAVING COUNT(*) > 1;
+WHERE cst_id IS NOT NULL) t
+WHERE flag_last = 1;
 
 
--- checking for unwanted spaces in string values
--- No result
-
-SELECT cst_firstname
+-- Transforming Extra space issues in cst_firstname & cst_lastname:
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 FROM bronze.crm_cust_info
-WHERE cst_firstname != TRIM(cst_firstname);
+WHERE cst_id IS NOT NULL) t
+WHERE flag_last = 1;
 
+-- Transforming: changing the gndr mapping from abbreviation to full naming for cst_gndr and cst_marital_status
 
-SELECT cst_lastname
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+     WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+     ELSE 'N/a' END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+     WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+     ELSE 'N/a' END cst_gndr,
+cst_create_date
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 FROM bronze.crm_cust_info
-WHERE cst_lastname != TRIM(cst_lastname);
+WHERE cst_id IS NOT NULL) t
+WHERE flag_last = 1;
 
-SELECT cst_gndr
+-- Insert into the silver table
+
+INSERT INTO silver.crm_cust_info (
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date)
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+     WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+     ELSE 'N/a' END cst_marital_status, -- Normalise marital status values to readable format
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+     WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+     ELSE 'N/a' END cst_gndr, -- Normalize gender values to readable format
+cst_create_date
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 FROM bronze.crm_cust_info
-WHERE cst_gndr != TRIM(cst_gndr);
-
--- Outcome reveals there are with space in both columns cst_firstname & cst_lastname
-
-
--- Data Standardization & Consistency
-
-SELECT DISTINCT cst_gndr
-FROM bronze.crm_cust_info;
-
-SELECT DISTINCT cst_marital_status
-FROM bronze.crm_cust_info;
-
+WHERE cst_id IS NOT NULL) t
+WHERE flag_last = 1; -- Select the most recent record per customer
 
 
 -- Exploring: data quanlity issues in the table: bronze.crm_prd_info
@@ -75,116 +137,360 @@ FROM bronze.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
--- Check for unwanted spaces
--- Expectation: No result
+-- Deriving new column from prd_key
 
-SELECT prd_nm
-FROM bronze.crm_prd_info
-WHERE prd_nm != TRIM(prd_nm);
-
-SELECT *
+SELECT
+prd_id,
+prd_key,
+SUBSTRING(prd_key, 1, 5) AS cat_id,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
 FROM bronze.crm_prd_info;
 
--- Check for Nulls or Negative Numbers in the column prd_cost
+-- exploring the cat_id from bronze.erp_px_cat_g1v2
 
-SELECT prd_cost
-FROM bronze.crm_prd_info
-WHERE prd_cost IS NULL OR prd_cost < 0;
+SELECT DISTINCT id
+FROM bronze.erp_px_cat_g1v2;
 
-
--- Data Standardization & consistency fpor column prd_line
-SELECT DISTINCT prd_line
-FROM bronze.crm_prd_info; -- 4 DISTINCT Value including Null
-
-
--- check for invalid Date Orders
-SELECT *
-FROM bronze.crm_prd_info
-WHERE prd_start_dt > prd_end_dt;
--- Outcome reveals some start dates are greater than end_date, which is invalid
-
-
--- Exploring: data quanlity issues in the table: bronze.crm_sales_details
-
--- finding unwanted spaces in sls_ord_num
-
-SELECT *
-FROM bronze.crm_sales_details
-WHERE sls_ord_num != TRIM(sls_ord_num);
-
--- Check for invalid dates
+-- outcome reveals cat_id in "bronze.crm_prd_info" is stored with '-' while id in "bronze.erp_px_cat_g1v2" is stored with '_'
+-- using replace function 
 
 SELECT
-sls_order_dt
-FROM bronze.crm_sales_details
-WHERE sls_order_dt < 0;
+prd_id,
+prd_key,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+FROM bronze.crm_prd_info;
+
+-- creating a new column off the remaining string in prd_key
+SELECT
+prd_id,
+prd_key,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+FROM bronze.crm_prd_info;
+
+-- Replacing Null values in column prd_cost with N/a
 
 SELECT
-sls_order_dt
-FROM bronze.crm_sales_details
-WHERE sls_order_dt <= 0;
--- Outcome shows Date column contains 0, converto Null
+prd_id,
+prd_key,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+FROM bronze.crm_prd_info;
 
--- Outcome also reveals, data is stored as INT, lENGTH = 8
 
--- Check for date length
+-- Transforming: changing the prd_line mapping from abbreviation to full naming for prd_line
+
 SELECT
-sls_order_dt
-FROM bronze.crm_sales_details
-WHERE sls_order_dt <= 0 OR LEN(sls_order_dt) !=8 OR sls_order_dt > 20500101 -- this simply means sls_order_dt > 2050/01/01;
--- There are 19 values that looks to to be invalid
+prd_id,
+prd_key,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+     WHEN 'M' THEN 'Mountain'
+     WHEN 'R' THEN 'Road'
+     WHEN 'S' THEN 'Other Sales'
+     WHEN 'T' THEN 'Touring'
+     ELSE 'N/a' 
+     END AS prd_line,
+prd_start_dt,
+prd_end_dt
+FROM bronze.crm_prd_info;
 
 
--- Check for Invalid sls_order_dt
+-- Fixing the invalid date columns
+-- setting rules: set end date as (Next start date cell, in the next row) - 1day and also start date can not be Null
+
+SELECT
+prd_id,
+prd_key,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+     WHEN 'M' THEN 'Mountain'
+     WHEN 'R' THEN 'Road'
+     WHEN 'S' THEN 'Other Sales'
+     WHEN 'T' THEN 'Touring'
+     ELSE 'N/a' 
+     END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt, 
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
+
+-- Insert Into Silver Table
+
+INSERT INTO silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract Cat_id
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Extract prd_key
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+     WHEN 'M' THEN 'Mountain'
+     WHEN 'R' THEN 'Road'
+     WHEN 'S' THEN 'Other Sales'
+     WHEN 'T' THEN 'Touring'
+     ELSE 'N/a' 
+     END AS prd_line, -- Map prd_line codes to descriptive values
+CAST(prd_start_dt AS DATE) AS prd_start_dt, 
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt -- Calculate end date as one day before the next date
+FROM bronze.crm_prd_info;
 
 SELECT *
-FROM bronze.crm_sales_details
-WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt;
-
--- checking for inconsistencies in sales, quantity and price
--- Business Rules (Sales = Quantity * Price)
--- Business Rules: value in this column must not be negative, zero, Null 
-
-SELECT 
-DISTINCT sls_sales, sls_quantity, sls_price
-FROM bronze.crm_sales_details
-WHERE sls_sales != sls_quantity * sls_price
-OR sls_sales IS NULL OR sls_quantity IS NULL or sls_price IS NULL
-OR sls_sales <= 0 OR sls_quantity <= 0 or sls_price <= 0
-ORDER BY sls_sales, sls_quantity, sls_price;
+FROM silver.crm_prd_info;
 
 
 
+-- Transforming Data Quality issue in bronze.crm_sales_details
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+     END AS sls_order_dt,
+sls_due_dt,
+sls_ship_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details;
 
--- Exploring bronze.erp_cust_az12
+-- Transforming other date columns sls_due_dt, sls_ship_dt
 
-SELECT *
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+     END AS sls_order_dt,
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+     END AS sls_due_dt,
+CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+     END AS sls_ship_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details;
+
+-- Transforming the column sales, quantity and price using the business rule: sales = quantity * price, also, sales, quantity
+-- and price is NOT NULL, Negative
+
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+     END AS sls_order_dt,
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+     END AS sls_due_dt,
+CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+     END AS sls_ship_dt,
+CASE WHEN sls_sales <= 0 OR sls_sales IS NULL OR sls_sales != sls_quantity * ABS(sls_price)
+     THEN sls_quantity * ABS(sls_price)
+     ELSE sls_sales
+     END AS sls_sales,
+CASE WHEN sls_price <= 0 OR sls_price IS NULL OR sls_price != sls_sales / NULLIF(sls_quantity, 0)
+     THEN sls_sales / NULLIF(sls_quantity, 0)
+     ELSE sls_price
+     END AS sls_price,
+CASE WHEN sls_quantity IS NULL OR sls_quantity <= 0 OR sls_quantity != sls_sales / NULLIF(sls_price, 0)
+     THEN sls_sales / NULLIF(sls_price, 0)
+     ELSE sls_quantity
+     END AS sls_quantity
+FROM bronze.crm_sales_details;
+
+-- INSERT INTO SILVER TABLE
+
+INSERT INTO silver.crm_sales_details (
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+)
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+     END AS sls_order_dt,
+     CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+     END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+     END AS sls_due_dt,
+CASE WHEN sls_sales <= 0 OR sls_sales IS NULL OR sls_sales != sls_quantity * ABS(sls_price)
+     THEN sls_quantity * ABS(sls_price)
+     ELSE sls_sales
+     END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+CASE WHEN sls_quantity IS NULL OR sls_quantity <= 0 OR sls_quantity != sls_sales / NULLIF(sls_price, 0)
+     THEN sls_sales / NULLIF(sls_price, 0)
+     ELSE sls_quantity
+     END AS sls_quantity,-- Recalculate quantity if original value is missing or incorrect
+CASE WHEN sls_price <= 0 OR sls_price IS NULL OR sls_price != sls_sales / NULLIF(sls_quantity, 0)
+     THEN sls_sales / NULLIF(sls_quantity, 0)
+     ELSE sls_price
+     END AS sls_price -- Recalculate price if original value is missing or incorrect
+FROM bronze.crm_sales_details;
+
+
+
+-- Transforming data quanlity issue in bronze.erp_cust_az12
+-- eliminate string 'NAS' from column cid
+
+SELECT
+bdate,
+gen,
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+     ELSE cid
+     END AS cid
 FROM bronze.erp_cust_az12;
 
--- cid contains string 'NAS', eliminate this string from the rest
+-- Fixing invalid bdate
+SELECT
+gen,
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+     ELSE cid
+     END AS cid,
+CASE WHEN bdate > GETDATE() THEN NULL
+     ELSE bdate
+     END AS bdate
+FROM bronze.erp_cust_az12;
 
--- Investigating the bdate range
-SELECT DISTINCT
-bdate
-FROM bronze.erp_cust_az12
-WHERE bdate < '1924-01-01' OR bdate > GETDATE();
 
--- Inestigating gen column
-
+-- fixing inconsistency in the values in gen
 SELECT DISTINCT gen
 FROM bronze.erp_cust_az12;
 
--- Exploring bronze.erp_1oc_a101
-SELECT *
-FROM bronze.erp_1oc_a101;
+SELECT
+CASE(UPPER(TRIM(gen)))
+     WHEN 'F' THEN 'Female'
+     WHEN 'M' THEN 'Male'
+     ELSE gen
+     END AS gen,
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+     ELSE cid
+     END AS cid,
+CASE WHEN bdate > GETDATE() THEN NULL
+     ELSE bdate
+     END AS bdate
+FROM bronze.erp_cust_az12;
 
--- Appears the character '-' needs to be eliminated from the cid column
+-- Insert Into Silver table
 
+INSERT INTO Silver.erp_cust_az12 (
+gen,
+cid,
+bdate
+)
+SELECT
+CASE WHEN UPPER(TRIM(gen)) IN ('F', 'Female') THEN 'Female'
+     WHEN UPPER(TRIM(gen)) IN ('M', 'Male') THEN 'Male'
+     ELSE 'n/a'
+     END AS gen,
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+     ELSE cid
+     END AS cid,
+CASE WHEN bdate > GETDATE() THEN NULL
+     ELSE bdate
+     END AS bdate
+FROM bronze.erp_cust_az12;
+
+
+-- Transforming data quanlity issue in bronze.erp_1oc_a101
+-- Removing the character '-' from the cid column
+
+SELECT
+REPLACE(cid, '-', '') AS cid,
+cntry
+FROM Bronze.erp_1oc_a101;
+
+-- Fixing the inconsistency in cntry description
 SELECT DISTINCT cntry
 FROM bronze.erp_1oc_a101;
 
+SELECT
+REPLACE(cid, '-', '') AS cid,
+CASE WHEN TRIM(cntry) IN ('DE', 'Germany') THEN 'Germany'
+     WHEN TRIM(cntry) IN ('USA', 'US', 'United States') THEN 'USA'
+     WHEN (cntry) IS NULL OR cntry = '' THEN 'n/a'
+     ELSE TRIM(cntry)
+     END AS cntry
+FROM Bronze.erp_1oc_a101;
 
--- Exploring bronze.erp_px_cat_g1v2;   cat, subcat, maintenance
+-- Insert into Silver table
 
-SELECT *
+INSERT INTO Silver.erp_1oc_a101 (
+cid,
+cntry)
+SELECT
+REPLACE(cid, '-', '') AS cid,
+CASE WHEN TRIM(cntry) IN ('DE', 'Germany') THEN 'Germany'
+     WHEN TRIM(cntry) IN ('USA', 'US', 'United States') THEN 'USA'
+     WHEN (cntry) IS NULL OR cntry = '' THEN 'n/a'
+     ELSE TRIM(cntry)
+     END AS cntry
+FROM Bronze.erp_1oc_a101;
+
+
+
+-- Transforming data quanlity issue in bronze.erp_px_cat_g1v2;
+
+-- Insert into the Silver table
+
+INSERT INTO Silver.erp_px_cat_g1v2 (
+id, cat, subcat, maintenance)
+SELECT 
+id,
+cat,
+subcat,
+maintenance
 FROM bronze.erp_px_cat_g1v2;
+
+ 
